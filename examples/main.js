2b8f10eefd37b2e4864450e153f403ba7ab74bdb
Li Ge <lge@ucsd.edu>
2013-10-03 19:04:53 -0700

    Update at Thu Oct  3 19:04:53 PDT 2013

--- examples/main.js
+++ examples/main.js
@@ -1,9 +1,49 @@
 var EditorControls = require( 'ise-editor-controls' );
 var ISEViewport = require( 'ise-viewport' );
 var randomCubes = require( 'three-random-cubes' );
+var THREE = require( 'three' );
+var StrokeRenderer = require( 'ise-stroke-renderer' );
 
 var viewport = ISEViewport();
 var controls = new EditorControls( viewport.camera, viewport.container );
+var strokeRenderer = new StrokeRenderer( viewport.canvas2D );
+strokeRenderer.start();
 
 var scene = viewport.scene;
 var cubes = randomCubes( 100 ).map( function( c ) { scene.add( c ); return c; } );
+
+var currentSketch = [];
+var tid = null;
+controls.on( 'sketchStart', function( p ) {
+  // currentSketch = [];
+  clearTimeout( tid );
+  strokeRenderer.add( currentSketch );
+  // var c = cube();
+  // c.position.y = p.x;
+  // c.position.z = p.y;
+  // scene.add( c );
+} );
+
+controls.on( 'sketching', function( p ) {
+  currentSketch.push( p );
+  // var c = cube();
+  // c.position.y = p.x;
+  // c.position.z = p.y;
+  // scene.add( c );
+} );
+
+controls.on( 'oneStroke', function( p ) {
+  tid = setTimeout( function() {
+    strokeRenderer.clear();
+  }, 1000 );
+  currentSketch = [];
+} );
+
+
+function cube( w, h, t ) {
+  var m = new THREE.Mesh(
+    new THREE.CubeGeometry( w || 40, h || 40, t || 40 )
+  );
+  m.material.wireframe = false;
+  return m;
+};
