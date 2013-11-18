7399f888d32b104d76344aa71ec66bb8f0cc61e9
Li Ge <lge@ucsd.edu>
2013-11-18 05:07:14 -0800

    Update at Mon Nov 18 05:07:14 PST 2013

--- examples/main.js
+++ examples/main.js
@@ -4,10 +4,41 @@ var randomCubes = require( 'three-random-cubes' );
 var THREE = require( 'three' );
 var StrokeRenderer = require( 'ise-stroke-renderer' );
 
+
+window.onError = function( msg, url ) {
+  document.getElementById( 'debug' ).textContent = msg;
+}
+
+document.getElementById( 'toggle-sketch' ).addEventListener( 'click', function() {
+  controls.sketching = !controls.sketching;
+  alert('toggle');
+} );
+
+// document.getElementById( 'toggle-sketch' ).addEventListener( 'mouseup', function() {
+//   controls.sketching = false;
+// } );
+
+
 var viewport = ISEViewport( { container: document.getElementById( 'main' ), grid: true } );
 var controls = new EditorControls( viewport.camera, viewport.container );
 var strokeRenderer = new StrokeRenderer( viewport.canvas2D );
 strokeRenderer.start();
+
+var grid = viewport.grid;
+// var mat = new THREE.Matrix4();
+// mat.makeRotationX( Math.PI/2 );
+// grid.geometry.applyMatrix( mat );
+// grid.geometry.verticesNeedUpdate = true;
+// grid.rotateX( Math.PI/2 );
+
+// viewport.camera.position.set( 200, 200, 200 );
+
+var p = new THREE.PlaneGeometry( 200, 200 );
+var m = new THREE.MeshBasicMaterial( {
+  color: 0xff0000
+} );
+var pp = new THREE.Mesh( p, m );
+viewport.scene.add( pp );
 
 var scene = viewport.scene;
 var cubes = randomCubes( 100 ).map( function( c ) { scene.add( c ); return c; } );
@@ -29,7 +60,6 @@ controls.on( 'oneStroke', function( p ) {
   }, 1000 );
   currentSketch = [];
 } );
-
 
 function cube( w, h, t ) {
   var m = new THREE.Mesh(
